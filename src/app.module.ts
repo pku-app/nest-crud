762290f4a5ef578ca8faf37cfaaf71601357fd18
Utzig26 <gabriel.utzig26@gmail.com>
2022-05-24 16:53:39 -0300

    remove 'hello world' service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,19 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(
       process.env.MONGO_URI,
     ),
     AuthModule,
+    UserModule,
   ],
 })
 export class AppModule {}
